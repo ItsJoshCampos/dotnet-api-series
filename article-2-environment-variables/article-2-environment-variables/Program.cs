75200fcd728ece49f22be4be056a5712bd66fd5f
Josh Campos <30943683+ItsJoshCampos@users.noreply.github.com>
2023-03-02 22:03:15 -0700

    Add Article 2 Project (#2)

---
+++ article-2-environment-variables/article-2-environment-variables/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace article_2_environment_variables;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.Configure<SampleClass>(builder.Configuration.GetSection(nameof(SampleClass)));
+
+        var app = builder.Build();
+
+        var vs_env = Environment.GetEnvironmentVariable("VS_ENV");
+        var macos_env = Environment.GetEnvironmentVariable("MACOS_ENV");
+
+        var dict = Environment.GetEnvironmentVariables();
+        //Add Breakpoint on next line
+        foreach (var d in dict)
+        {
+            Console.WriteLine(d);
+        }
+
+        var appsettings_env_sq_brackets = builder.Configuration["MyENV"];
+        var appsettings_env_get_value = builder.Configuration.GetValue<string>("MyENV");
+
+        
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
+
---
+++ article-2-environment-variables/article-2-environment-variables/SampleClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace article_2_environment_variables;
+
+public class SampleClass
+{
+	public string StringValue { get; set; }
+	public bool BooleanValue { get; set; }
+}
