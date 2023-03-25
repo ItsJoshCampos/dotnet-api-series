83ecc514949568b9ee73109aa50cae394ea3d23c
Josh Campos <30943683+ItsJoshCampos@users.noreply.github.com>
2023-03-25 15:25:52 -0600

    Add Article 3 (#3)

---
+++ article-3-api-key/article-3-api-key/Authentication/AuthApiKeyMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace article_3_api_key.Authentication
+{
+	public class AuthApiKeyMiddleware
+	{
+		// Request Delete that is used to manage each HTTP request.
+		private readonly RequestDelegate _next;
+		// API Key Header and ENV Name
+		private const string ApiKeyHeader = "x-api-key";
+
+		// Inject Request Delegate into API Key Middleware
+		public AuthApiKeyMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		// Pass the Request's HTTP Context
+		public async Task InvokeAsync(HttpContext context)
+		{
+			// Look for the "x-api-key" Header in the request
+			if(!context.Request.Headers.TryGetValue(ApiKeyHeader, out var extractedApiKey))
+			{
+				// If not found, throw a 401 status
+				// 401 = Invalid or Missing Credentials
+				// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+				context.Response.StatusCode = 401;
+				await context.Response.WriteAsync("UNAUTHORIZED: API Key Missing");
+				return;
+			}
+
+			// Get the real key from our ENV Variable
+            var apiVal = Environment.GetEnvironmentVariable(ApiKeyHeader);
+
+			// API Key found in Header
+			// Validate the Key
+			if(!apiVal.Equals(extractedApiKey))
+			{
+                // If key is not valid, throw a 401 status
+                // 401 = Invalid or Missing Credentials
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+                context.Response.StatusCode = 401;
+				await context.Response.WriteAsync("UNAUTHORIZED: Invalid API Key");
+			}
+
+			// Valid API Key was provided
+			// Pass request to the next delegate in the pipeline
+			await _next(context);
+		}
+	}
+}
+
---
+++ article-3-api-key/article-3-api-key/Program.cs
@@ -0,0 +1,57 @@
+﻿using article_3_api_key.Authentication;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.AddSecurityDefinition("Weather API Key", new OpenApiSecurityScheme
+    {
+        // Header Name
+        Name = "x-api-key",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "ApiKeyScheme",
+        In = ParameterLocation.Header,
+        Description = "API Key Header Requirement"
+    });
+
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Weather API Key"
+                },
+                // Scheme using Header
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseMiddleware<AuthApiKeyMiddleware>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
