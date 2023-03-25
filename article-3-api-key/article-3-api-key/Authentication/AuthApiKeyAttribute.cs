cf4a105fcaa7c061bf66daa34786da1bc4dc8897
itsjoshcampos <itsjoshcampos@Joshs-MacBook-Pro.local>
2023-03-25 15:25:19 -0600

    Add Article 3

---
+++ article-3-api-key/article-3-api-key/Authentication/AuthApiKeyAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace article_3_api_key.Authentication
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthApiKeyAttribute : Attribute, IAsyncActionFilter
+	{
+        // API Key Header and ENV Name
+        private const string ApiKeyHeader = "x-api-key";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // Look for the "x-api-key" Header in the request
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeader, out var apiKeyVal))
+            {
+                // If not found, throw a 401 status
+                // 401 = Invalid or Missing Credentials
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+                context.HttpContext.Response.StatusCode = 401;
+                await context.HttpContext.Response.WriteAsync("UNAUTHORIZED: API Key Missing");
+            }
+
+            // Get the real key from our ENV Variable
+            var apiVal = Environment.GetEnvironmentVariable(ApiKeyHeader);
+            if (!apiVal.Equals(apiKeyVal))
+            {
+                // If not found, throw a 401 status
+                // 401 = Invalid or Missing Credentials
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+                context.HttpContext.Response.StatusCode = 401;
+                await context.HttpContext.Response.WriteAsync("UNAUTHORIZED: Invalid API Key");
+            }
+
+            await next();
+        }
+    }
+}
+
---
+++ article-3-api-key/article-3-api-key/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using article_3_api_key.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace article_3_api_key.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [AuthApiKey]
+    [HttpGet]
+    [Route("Secure")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpGet]
+    [Route("Open")]
+    public IEnumerable<WeatherForecast> GetTest()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
